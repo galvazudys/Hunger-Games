b72db5df8affb816bb09bbd0d8617b2673ffbfda
galvazudys <golfas705@yahoo.com>
2017-08-29 11:19:28 +0100

    Refactor and added comments

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 let app = document.getElementById('app');
-let calOutput = document.getElementById('output');
+let caloriesOutput = document.getElementById('output');
 let ulNode = document.createElement('ul');
-let totalOutput = document.getElementById('totalCalories');
-let newData = [];
+let totalCaloriesOutput = document.getElementById('totalCalories');
+let newListOfProducts = [];
 let totalCal = 0;
 let data;
 
 ulNode.setAttribute('class', 'collection');
 app.appendChild(ulNode);
 
+//using event listiner onchange placed directly on input tag and passing value
+//this function handle event.
 function fetchData(val) {
     ulNode.innerHTML = `<div class="progress">
                           <div class="indeterminate"></div>
                         </div>`;
+
+    //using AJAX request to api server,that gives as a promise
     axios({
         method: 'get',
         url: `https://nutritionix-api.p.mashape.com/v1_1/search/${val}?fields=nf_calories,item_name,brand_name`,
@@ -21,6 +25,7 @@ function fetchData(val) {
                 'LOkyvYzwXFmshynyglg0VkGgbg8np1gtcWyjsnDDvOGdcGCvhu'
         }
     }).then(function(response) {
+        //handle promise callback
         ulNode.innerHTML = '';
         data = response.data.hits;
         console.log(data);
@@ -36,33 +41,36 @@ function fetchData(val) {
     });
 }
 
+//event listener for handling adding object to new list of products
 ulNode.addEventListener('click', function(e) {
-    calOutput.innerHTML = '';
+    caloriesOutput.innerHTML = '';
     data.forEach(item => {
         if (item._id === e.target.id) {
-            newData.push({
+            newListOfProducts.push({
                 name: item.fields.item_name,
                 calories: item.fields.nf_calories
             });
-            totalListRender(newData);
+            renderList(newListOfProducts);
         }
     });
 });
 
-calOutput.addEventListener('click', function(e) {
+//event Listener for handling removing object from new product list
+caloriesOutput.addEventListener('click', function(e) {
     if (e.target.tagName === 'I') {
-        calOutput.innerHTML = '';
+        caloriesOutput.innerHTML = '';
         let targetId = parseInt(e.target.parentNode.parentNode.id);
-        newData.splice(targetId, 1);
-        totalListRender(newData);
+        newListOfProducts.splice(targetId, 1);
+        renderList(newListOfProducts);
     }
 });
 
-function totalListRender(data) {
-    data.forEach((item, index) => {
+function renderList(value) {
+    totalCal = 0;
+    value.forEach((item, index) => {
         totalCal += item.calories;
-        calOutput.innerHTML += `<li id=${index} class="collection-item"><span><i class="fa fa-times"></i></span> ${item.name}<span class="right calories-out">${item.calories}</span></li>`;
-        totalOutput.innerHTML = `<hr><h4><em>Total Calories: </em> ${parseInt(
+        caloriesOutput.innerHTML += `<li id=${index} class="collection-item"><span><i class="fa fa-times"></i></span> ${item.name}<span class="right calories-out">${item.calories}</span></li>`;
+        totalCaloriesOutput.innerHTML = `<hr><h4><em>Total Calories: </em> ${parseInt(
             totalCal
         )}</h4>`;
     });
